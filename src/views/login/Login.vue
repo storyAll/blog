d00814221d499582ed525bcdb27698a792787f95
Zhang Chong <zcychongyao2008@sina.com>
2018-09-09 00:20:24 +0800

    vue初始化项目

---
+++ src/views/login/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <el-row>
+      <el-input  placeholder="用户名" type="text" auto-complete label="用户名"></el-input>
+    </el-row>
+    <el-row>
+      <el-input  placeholder="密码" type="password" label="密码"></el-input>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
